68b8a31efd495c352d407117fe10d6f826bd26ae
Debora Donadello <debora.donadello@outlook.com>
2022-12-10 03:20:19 +0100

    Calculates max calories - first puzzle part of day 1 is solved

--- adv-12-01.js
+++ adv-12-01.js
@@ -1,13 +1,29 @@
+//imports text reader
 const fs = require('fs')
 
-fs.readFile('adv-12-01-input.txt', readFile)
-
-function readFile(err, data) {
-    if (err) throw err;
-    return data.toString();
+//reads text and splits values into arrays according to gap paragraphs
+//adds blank spaces as 0 calories
+//adds all calories
+function sumCalories() {
+    fs.readFile("adv-12-01-input.txt", (err, data) => {
+        const calories = data.toString().split(/\r?\n/)
+        let elfCalories = 0
+        const allCalories = []
+        for (let calorie of calories) {
+            if (calorie === "") {
+                allCalories.push(elfCalories)
+                elfCalories = 0
+            } else {
+                elfCalories += parseInt(calorie)
+            }
+        }
+        //calculates max calories
+        const maxCalories = Math.max(...allCalories)
+        console.log(maxCalories)
+    })
 }
 
-
+sumCalories()
 
 
 
