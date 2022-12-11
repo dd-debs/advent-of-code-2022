4b4d95bbd441af717d5b16f48475c86a7e79f5a1
Debora Donadello <debora.donadello@outlook.com>
2022-12-12 00:46:11 +0100

    Set final structure for loops

--- day-3/adv-03-pt1.js
+++ day-3/adv-03-pt1.js
@@ -3,15 +3,27 @@ function setPriorities() {
     fs.readFile('adv-12-03-input.txt', (err, data) => {
         const entries = data.toString();
         const backpacks = entries.split(/\n/)
+        let backpackFinal = []
+        let firstCompartment = ""
+        let secondCompartment = ""
         //console.log(backpacks)
         for (let backpack of backpacks) {
-            let firstCompartment = backpack.slice(0, Math.floor(backpack.length/2));
-            let secondCompartment = backpack.slice(Math.floor(backpack.length/2), backpack.length)
-            let backpackFinal = []
+            firstCompartment = backpack.slice(0, Math.floor(backpack.length/2));
+            secondCompartment = backpack.slice(Math.floor(backpack.length/2), backpack.length)
             backpackFinal.push(firstCompartment)
             backpackFinal.push(secondCompartment)
-            //console.log(backpackFinal)
-            //TODO: loop through the elements of the arrays to find which letter is common & assign priority points (a-z -> 1-26 / A-Z -> 27-52)
+        }
+        let partitions = []
+        for (let i=0; i<backpackFinal.length;i+=2) {
+            partitions.push(backpackFinal.slice(i,i+2))
+        }
+        //console.log(partitions);
+        //loops through the sub-arrays
+        for (let i=0; i<partitions.length; i++) {
+            const innerArray = partitions[i].length;
+            for (let j=0; j<innerArray; j++) {
+                //TODO: loop through the elements of the arrays to find which letter is common & assign priority points (a-z -> 1-26 / A-Z -> 27-52)
+            }
         }
 
     })
