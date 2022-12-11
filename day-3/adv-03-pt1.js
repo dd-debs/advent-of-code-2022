94ae207f16de3ced844e6a98d9e99021abff2c50
Debora Donadello <debora.donadello@outlook.com>
2022-12-11 19:13:29 +0100

    Initial commit

--- day-3/adv-03-pt1.js
+++ day-3/adv-03-pt1.js
@@ -1,1 +1,8 @@
+function setPriorities() {
+    const fs = require('fs')
+    fs.readFile('adv-12-03-input.txt', (err, data) => {
+        const backpacks = data.toString();
+        console.log(backpacks)
 
+    })
+}
