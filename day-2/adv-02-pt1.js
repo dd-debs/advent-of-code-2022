4ba9441756b462607e7038d1692c91b218131acc
Debora Donadello <debora.donadello@outlook.com>
2022-12-11 17:59:57 +0100

    Solved first part of Day 2 puzzle

--- day-2/adv-02-pt1.js
+++ day-2/adv-02-pt1.js
@@ -1,10 +1,68 @@
-const fs = require('fs')
-
-fs.readFile('adv-12-02-input.txt', (err, data) => {
+function playGame() {
+    const fs = require('fs')
+    fs.readFile('adv-12-02-input.txt', (err, data) => {
     const entries = data.toString();
+
     //deletes the space between the letters of each couplet
     const entriesWithoutSpace = entries.replace(/ /g, '');
+
     //creates one big array where each couplet is an element of the array
     const macroArray = entriesWithoutSpace.split(/\n/)
-    console.log(macroArray);
-})
+
+    let totalScore = []
+        for (let round of macroArray) {
+            let shapeScore = 0;
+            let outcomeScore = 0;
+            let totalRoundScore = 0;
+            let opponentMove = round.charAt(0)
+            let myMove = round.charAt(1)
+            if (opponentMove === 'A' && myMove === 'X') {
+                shapeScore += 1;
+                outcomeScore += 3;
+                totalRoundScore++
+            } else if (opponentMove === 'A' && myMove === 'Y') {
+                shapeScore += 2;
+                outcomeScore += 6;
+                totalRoundScore++
+            } else if (opponentMove === 'A' && myMove === 'Z') {
+                shapeScore += 3;
+                outcomeScore += 0;
+                totalRoundScore++
+            }
+            if (opponentMove === 'B' && myMove === 'X') {
+                shapeScore += 1;
+                outcomeScore += 0;
+                totalRoundScore++
+            } else if (opponentMove === 'B' && myMove === 'Y') {
+                shapeScore += 2;
+                outcomeScore += 3;
+                totalRoundScore++
+            } else if (opponentMove === 'B' && myMove === 'Z') {
+                shapeScore += 3;
+                outcomeScore += 6;
+                totalRoundScore++
+            }
+            if (opponentMove === 'C' && myMove === 'X') {
+                shapeScore += 1;
+                outcomeScore += 6;
+                totalRoundScore++
+            } else if (opponentMove === 'C' && myMove === 'Y') {
+                shapeScore += 2;
+                outcomeScore += 0;
+                totalRoundScore++
+            } else if (opponentMove === 'C' && myMove === 'Z') {
+                shapeScore += 3;
+                outcomeScore += 3;
+                totalRoundScore++
+            }
+            totalRoundScore = shapeScore + outcomeScore
+
+            totalScore.push(totalRoundScore)
+        }
+        //sums the scores from each round to find the total -> 13809
+        const sum = totalScore.reduce((partialSum, a) => partialSum + a, 0);
+        console.log(sum);
+    })
+}
+
+playGame();
