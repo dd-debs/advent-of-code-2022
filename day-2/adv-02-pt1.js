aef8b52b98dc047ffcf72602e09921eb41771005
Debora Donadello <debora.donadello@outlook.com>
2022-12-11 18:02:51 +0100

    Layout fixes and comments

--- day-2/adv-02-pt1.js
+++ day-2/adv-02-pt1.js
@@ -2,14 +2,12 @@ function playGame() {
     const fs = require('fs')
     fs.readFile('adv-12-02-input.txt', (err, data) => {
     const entries = data.toString();
-
     //deletes the space between the letters of each couplet
     const entriesWithoutSpace = entries.replace(/ /g, '');
-
     //creates one big array where each couplet is an element of the array
     const macroArray = entriesWithoutSpace.split(/\n/)
-
     let totalScore = []
+
         for (let round of macroArray) {
             let shapeScore = 0;
             let outcomeScore = 0;
