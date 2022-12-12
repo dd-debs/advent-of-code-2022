e8e77240f27a762bd3e2b4008bb73d54137398df
Debora Donadello <debora.donadello@outlook.com>
2022-12-12 15:03:19 +0100

    Solved first part of Day 3 puzzle

--- day-3/adv-03-pt1.js
+++ day-3/adv-03-pt1.js
@@ -3,43 +3,60 @@ function setPriorities() {
     fs.readFile('adv-12-03-input.txt', (err, data) => {
         const entries = data.toString();
         const backpacks = entries.split(/\n/)
-        let backpackFinal = []
-        let firstCompartment = ""
-        let secondCompartment = ""
-        //console.log(backpacks)
+        let backpackFinal = [];
+        let partitions = [];
+
+        let firstCompartment = "";
+        let secondCompartment = "";
+        //divides each string into two to get the equal compartments of each backpack
         for (let backpack of backpacks) {
             firstCompartment = backpack.slice(0, Math.floor(backpack.length/2));
             secondCompartment = backpack.slice(Math.floor(backpack.length/2), backpack.length)
             backpackFinal.push(firstCompartment)
             backpackFinal.push(secondCompartment)
         }
-        let partitions = []
+
+        //creates a multidimensional array with an outer array and multiple inner arrays, each with the two comps of each backpack
         for (let i=0; i<backpackFinal.length;i+=2) {
             partitions.push(backpackFinal.slice(i,i+2))
         }
-        //console.log(partitions);
-        //loops through the sub-arrays
-        for (let i=0; i<partitions.length; i++) {
-            const innerArray = partitions[i].length;
-            for (let j=0; j<innerArray; j++) {
-                //TODO: loop through the elements of the arrays to find which letter is common & assign priority points (a-z -> 1-26 / A-Z -> 27-52)
-                /*var cubes = [
-                 [1, 2, 3],
-                 [4, 5, 6],
-                 [7, 8, 9],
-                ];
 
-                cubes.forEach(function each(item) {
-                  if (Array.isArray(item)) {
-                    // If is array, continue repeat loop
-                    item.forEach(each);
-                  } else {
-                    console.log(item);
-                  }
-                });*/
-            }
+        //defines alphabet letters priorities
+        let alphabet = [];
+        for (let i=0; i<26; i++) {
+            alphabet.push({letter:String.fromCharCode(i+97), priority: i+1});
+            alphabet.push({letter: String.fromCharCode(i+65), priority: i+27});
         }
 
-    })
+        let commonLetters = []
+
+        //separates the letters of each compartment
+        for (let i=0; i<partitions.length; i++) {
+            const innerArray = partitions[i];
+            const letters1 = innerArray[0].split('');
+            const letters2 = innerArray[1].split('');
+            //checks if the two elements of each inner array have letters in common (!== index -1)
+            for (let j = 0; j<letters1.length; j++) {
+                if (letters2.indexOf(letters1[j]) !== -1) {
+                    commonLetters.push(letters1[j]);
+                    break;
+                }
+            }
+        }
+        //creates an array with common letters and respective priorities taken from the alphabet array
+        let resultsArray = commonLetters.map(letter => {
+            let alphabetObject = alphabet.find(item => item.letter === letter);
+            return {
+                letter: letter,
+                priority: alphabetObject.priority
+            }
+        });
+        //sums the priorities of the common letters to get the final result -> 8153
+        const sumOfPriorities = resultsArray.reduce((total, obj) => {
+            return total + obj.priority
+        },0);
+        console.log(sumOfPriorities);
+    });
 }
-setPriorities()
+setPriorities();
+
