6d941ee1e0f7b15facdd84ec40a23bd96baa9f4f
Debora Donadello <debora.donadello@outlook.com>
2022-12-12 19:54:27 +0100

    Initial commit for part 2

--- day-3/adv-03-pt2.js
+++ day-3/adv-03-pt2.js
@@ -1,1 +1,47 @@
+function setPriorities() {
+    const fs = require('fs')
+    fs.readFile('adv-12-03-input.txt', (err, data) => {
+        const entries = data.toString();
+        const backpacks = entries.split(/\n/)
+        //console.log(backpacks)
+        //let backpackFinal = [];
+        //let partitions = [];
+        const groupItems = 3;
 
+        const backpackGroups = backpacks.reduce((resultArray, item, index) => {
+            const groupIndex = Math.floor(index / groupItems);
+            if (!resultArray[groupIndex]) {
+                resultArray[groupIndex] = []
+            }
+            resultArray[groupIndex].push(item)
+            return resultArray
+        }, [])
+
+        let commonLetters = new Set();
+        let letters1;
+        let letters2;
+        let letters3;
+        let innerArray;
+
+        //separates the letters of each compartment
+        for (let i = 0; i < backpackGroups.length; i++) {
+            innerArray = backpackGroups[i];
+            letters1 = innerArray[0].split("");
+            letters2 = innerArray[1].split("");
+            letters3 = innerArray[2].split("");
+
+            //checks if the three elements of each inner array have letters in common (!== index -1)
+            for (let j = 0; j < letters1.length; j++) {
+                for (let k = 0; k < letters2.length; k++) {
+                    for (let l = 0; l < letters3.length; l++) {
+                        if (letters1[j].includes(letters2[k]) && letters2[k].includes(letters3[l])) {
+                            commonLetters.add(letters2[k])
+                        }
+                    }
+                }
+            }
+        }
+        console.log(commonLetters);
+    })
+}
+setPriorities();
